6973e136086f941b76a4f6d161de221b2d88cb03
Ahmad Azeez <114791071+AhmadAzeez999@users.noreply.github.com>
2024-03-03 16:56:33 -0800

    Route changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
+import App from './App.jsx';
 import WebProjectsPage from '../pages/WebProjectsPage.jsx';
 
-import 
-{
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "websiteProjects",
-    element: <WebProjectsPage/>,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+  <React.StrictMode>
+    <BrowserRouter basename="/My-Portfolio/"> {/* Replace <REPO> with your repository name */}
+      <Routes> {/* Define your routes using the Routes component */}
+        <Route path="/" element={<App />} />
+        <Route path="websiteProjects" element={<WebProjectsPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
