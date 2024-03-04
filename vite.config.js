ae3af051a43b49dad5c24e00b719072e435896b8
Ahmad Azeez <114791071+AhmadAzeez999@users.noreply.github.com>
2024-03-03 16:14:29 -0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/My-Portfolio/",
   plugins: [react()],
-  build: {
-    outDir: 'build', // Change the output directory to 'build'
-  },
-});
+})
